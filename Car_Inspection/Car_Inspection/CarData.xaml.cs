9eae854cac26ad8358ae7065a750e344ea98f5ad
marat <fakhrutdinov.m5@gmail.com>
2022-11-02 23:00:11 +0300

    Add DB and 3 window

---
+++ Car_Inspection/Car_Inspection/CarData.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Inspection
+{
+    /// <summary>
+    /// Логика взаимодействия для CarData.xaml
+    /// </summary>
+    public partial class CarData : Window
+    {
+        public CarData()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.ShowDialog();
+        }
+    }
+}
---
+++ Car_Inspection/Car_Inspection/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Inspection
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToInspectionForm_Click(object sender, RoutedEventArgs e)
+        {
+            InspectionData inspectionData = new InspectionData();
+            this.Close();
+            inspectionData.ShowDialog();            
+        }
+
+        private void ToCarData_Click(object sender, RoutedEventArgs e)
+        {
+            CarData carData = new CarData();
+            this.Close();
+            carData.ShowDialog();
+        }
+    }
+}
